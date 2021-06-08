a7df92a11f702c3df4f68c60665f37fc4b7603f6
Adityav664 <adityav664@gmail.com>
2021-06-08 20:53:36 +0530

    adding file

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './search-box.css';
+
+export const Searchbox=({placeholder,handleChange})=>(
+    <input className='search' type="search" placeholder={placeholder} 
+    onChange={handleChange}/>
+)
